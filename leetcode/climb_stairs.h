1857cd97a5b7f07c635157b35cc3042fd816f4b3
maks-mishin <maks.mishinFZ@gmail.com>
2022-02-01 21:46:20 +0300

    solve 70. Climbing Stairs

---
+++ leetcode/climb_stairs.h
@@ -0,0 +1,20 @@
+// Author : Maks Mishin
+// Date   : 2/1/2022
+
+#ifndef ALGORITHM_TASKS_CLIMB_STAIRS_H
+#define ALGORITHM_TASKS_CLIMB_STAIRS_H
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        int first = 0, second = 1, fib = 0;
+        for (int i = 0; i < n; ++i) {
+            fib = first + second;
+            first = second;
+            second = fib;
+        }
+        return fib;
+    }
+};
+
+#endif //ALGORITHM_TASKS_CLIMB_STAIRS_H
